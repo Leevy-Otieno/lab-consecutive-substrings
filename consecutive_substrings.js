2d4577a62aa45d2264639f40008b54879ef2edc5
Leevy-Otieno <emmanuelonlineacc@gmil.com>
2026-01-07 09:16:29 +0300

    fiest commit

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,23 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  let current = "";
+
+  //substrings
+  for (let j = i; j < string.length; j++) {
+    current += string[j];
+    result.push(current);
+  }
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings("abc"));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings("a"));
 }
 
 module.exports = consecutiveSubstrings;
-
-
