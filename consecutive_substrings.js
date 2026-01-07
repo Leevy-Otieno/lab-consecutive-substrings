137a6b2f996c82c7473cab32cb1d5011c16f0ed6
Leevy-Otieno <emmanuelonlineacc@gmil.com>
2026-01-07 09:20:14 +0300

    first commit

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -11,27 +11,7 @@ function consecutiveSubstrings(string) {
 
 if (require.main === module) {
   // add your own tests in here
-  const consecutiveSubstrings = require("../consecutive_substrings");
 
-  test("returns an empty array when the input string is empty", () => {
-    expect(consecutiveSubstrings("").length).toBe(0);
-  });
-
-  test("returns an array containing one string when the input is one character", () => {
-    expect(consecutiveSubstrings("a")).toEqual(["a"]);
-  });
-
-  test("can handle many letters", () => {
-    expect(consecutiveSubstrings("ab")).toEqual(["a", "ab", "b"]);
-    expect(consecutiveSubstrings("abc")).toEqual([
-      "a",
-      "ab",
-      "abc",
-      "b",
-      "bc",
-      "c",
-    ]);
-  });
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings("abc"));
 
